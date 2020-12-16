d93b0d013aafdba72e4549915abccfc99712589e
ChrisBu1404 <73700198+ChrisBu1404@users.noreply.github.com>
2020-12-16 07:27:26 +0100

    Starter code
    starter code for the Trivia-API project for the Udacity Full stack web developer nanodgegree

---
+++ book-shelf/backend/flaskr/__init__.py
@@ -0,0 +1,90 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy #, or_
+from flask_cors import CORS
+import random
+
+from models import setup_db, Book
+
+BOOKS_PER_SHELF = 12
+
+# @TODO: General Instructions
+#   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there. 
+#     If you do not update the endpoints, the lab will not work - of no fault of your API code! 
+#   - Make sure for each route that you're thinking through when to abort and with which kind of error 
+#   - If you change any of the response body keys, make sure you update the frontend to correspond. 
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+  @app.route('/')
+  def hell():
+    return 'Hello world'
+
+  @app.route('/books', methods=['GET'])
+  def get_books():
+    page = request.args.get('page', 1, type=int)
+    start = (page -1) * BOOKS_PER_SHELF
+    end = start + BOOKS_PER_SHELF
+    books = Book.query.all()
+    if len(books) == 0:
+      abort(404)
+
+    formatted_books = [book.format() for book in books]
+
+    return jsonify({
+      'success': True,
+      'books': formatted_books[start:end],
+      'total_books': len(formatted_books)
+    })
+
+  
+
+  # @TODO: Write a route that will update a single book's rating. 
+  #         It should only be able to update the rating, not the entire representation
+  #         and should follow API design principles regarding method and route.  
+  #         Response body keys: 'success'
+  # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
+  @app.route('/books/<int:book_id>', methods=['PATCH','GET'])
+  def update_book_rating(book_id):
+    book = Book.query.filter(Book.id == book_id).one_or_none()
+    print(book_id)
+    
+    print(book)
+    rating = request.args.get('rating', book.id, type=int)
+    # update = request.args('update', False, type=bool)
+    print(rating)
+    # 'if update:'
+    book.rating = rating
+
+    book.update()
+
+    return jsonify({
+      'success': True
+    })
+
+  # @TODO: Write a route that will delete a single book. 
+  #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
+  #        Response body keys: 'success', 'books' and 'total_books'
+
+  # TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
+
+
+  # @TODO: Write a route that create a new book. 
+  #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
+  # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books. 
+  #       Your new book should show up immediately after you submit it at the end of the page. 
+  
+  return app
+
+    
